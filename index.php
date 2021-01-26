2c28765807b79b56c6918424cefd6f1414edcb75
Dailee Howard <Dhoward18@mail.greenriver.edu>
2021-01-26 17:55:17 -0500

    Changes to index page

--- index.php
+++ index.php
@@ -34,4 +34,45 @@
         echo $view->render('views/lunch.html');
     });
 
+    // Define a "lunch/sandwich" route
+    $f3->route('GET /lunch/sandwich', function()
+    {
+        $view = new Template();
+        echo $view->render('views/sandwich.html');
+    });
+
+    // Define a "breakfast" route with parameters
+    $f3->route('GET /breakfast/@item', function($f3, $params)
+    {
+        var_dump($params);
+        $menu = array('eggs', 'waffles', 'pancakes');
+        $item = $params['item'];
+
+        if (in_array($item, $menu))
+        {
+            switch($item)
+            {
+                case 'eggs':
+                    $view = new Template();
+                    echo $view->render('views/eggs.html');
+                    break;
+                case 'pancakes':
+                    echo "Swedish or American?";
+                    break;
+                case 'waffles':
+                    $f3->reroute("https://www.wafflehouse.com/");
+                    break;
+                default:
+                    $f3->error(404);
+            }
+        }
+        else
+        {
+            echo "Sorry, we don't serve $item";
+        }
+
+        //$view = new Template();
+        //echo $view->render('views/breakfast.html');
+    });
+
     $f3->run();
