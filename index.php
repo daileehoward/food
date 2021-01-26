b1f1bf75bedda3fc16fbbfd5da4bbcfb61e1e00a
Dailee Howard <Dhoward18@mail.greenriver.edu>
2021-01-26 14:39:16 -0500

    Added a home page with image

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
 
-    //Require the autoload file
+    // Require the autoload file
     require_once('vendor/autoload.php');
 
-    //Create an instance of the Base class
+    // Create an instance of the Base class
     $f3 = Base::instance();
     $f3->set('DEBUG', 3);
 
-    //Define a default route (home page)
+    // Define a default route (home page)
     $f3->route('GET /', function () {
-        echo "My Food Page";
-        //$view = new Template();
-        //echo $view->render('views/home.html');
+        //echo "My Food Page";
+        $view = new Template();
+        echo $view->render('views/home.html');
     });
 
     $f3->run();
